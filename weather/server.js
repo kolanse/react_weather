24bebc96dc4f5e78440b5e1252d2ea6ae7677a3c
Samuel Ochuba <68715929+kolanse@users.noreply.github.com>
2023-05-25 10:25:28 +0100

    removed cors

--- weather/server.js
+++ weather/server.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 
 // Create an instance of Express app
 const app = express();
-app.use(cors({
-  origin: ['http://localhost:3000']
-}))
+app.use(cors())
 
 // Set up middleware for parsing JSON request bodies
 app.use(express.json());
